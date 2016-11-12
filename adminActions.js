7eff0c34b84c27f84f74dd32cbec3ad9d6ca36d6
MIchael LeGrand <michael_legrand@yahoo.com>
2016-11-12 13:17:56 -0500

    Adding the admin actions too

---
+++ adminActions.js
@@ -0,0 +1,111 @@
+Skip to content
+This repository
+Search
+Pull requests
+Issues
+Gist
+ @michaellegrand
+ Unwatch 3
+  Star 0
+  Fork 0 michaellegrand/bad_batch_alert Private
+ Code  Issues 0  Pull requests 0  Projects 0  Wiki  Pulse  Graphs  Settings
+Branch: master Find file Copy pathbad_batch_alert/smsService/adminActions.js
+f5518b6  22 days ago
+@michaellegrand michaellegrand spelling... sigh.. I need way to test locally...
+2 contributors @michaellegrand @LeGrandPants
+RawBlameHistory     
+94 lines (81 sloc)  2.88 KB
+// 
+// A collection of functions related to special admin actions that can be triggered from an admin phone number*/
+//
+var AdminActions = function() {
+
+  var self = this;
+
+  var TWILIO_NUMBER = process.env.TWILIO_NUMBER;
+  var MY_NUMBER     = process.env.MY_NUMBER;
+  
+  //fires off a test alert to all the registered users
+  self.adminTestAlerts = function(g, res, client, sender, action)
+  {
+    //Query for all users and send them alerts.
+    console.log("adminTestAlerts " + action );
+    var region = action.charAt(2);//in test alert the 2nd character is the region so.. like this is alert region '⚠2'️ (2 because unicode)
+    var findQueryString = "SELECT * FROM users WHERE region = " + region;
+    console.log(findQueryString);
+    var findQuery = client.query(findQueryString);
+    findQuery.on('row', function(row) {
+      console.log(JSON.stringify(row));
+      var phoneNumber = g.cryptoHelper.decrypt(row.phone_number);
+      console.log(phoneNumber);
+      g.twilio.sendMessage({
+        to: phoneNumber,
+        from: TWILIO_NUMBER,
+        body: '⚠️ Overdose nearby, please be careful: http://health.baltimorecity.gov/Fentanyl ⚠️',
+        mediaUrl: "http://www.mike-legrand.com/BadBatchAlert/uplift.jpg"  
+      }, function (err) {
+        if (err) {
+          return next(err);
+        }
+      });
+    }).on('error', function() {
+      console.log("nobody in region " + region + " to alert.")
+    });
+ 
+  };
+
+  self.adminHelloWorld = function(g, res, client, sender, action)
+  {
+    console.log("adminHelloWorld");
+    g.twilio.sendMessage({
+      to: MY_NUMBER,
+      from: TWILIO_NUMBER,
+      body: '👋 Hello World 👋'
+    }, function (err) {
+      if (err) {
+        return next(err);
+      }
+    });
+  };
+
+  self.encryptUsers = function(g, res, client, sender, action)
+  {
+    console.log("encryptUsers");
+    var findQueryString = "SELECT * FROM users";
+    console.log(findQueryString);
+    var findQuery = client.query(findQueryString);
+    findQuery.on('row', function(row) {
+      console.log(JSON.stringify(row));
+      var cryptoNumber = g.cryptoHelper.encrypt(row.phone_number);
+      console.log(cryptoNumber);
+    });
+  }
+
+
+  //Special admin actions, like mass text etc.
+  self.doAdminAction = function(g, res, client, sender, action)
+  {
+    if (sender != MY_NUMBER) return false;//not admin sorry buddy.
+
+    if (action.startsWith("⚠️")) {//Alert Emoji
+      self.adminTestAlerts(g, res, client, sender, action);
+    } else if (action == "👋") {
+      self.adminHelloWorld(g, res, client, sender, action);
+    } else if (action == "Crypto") {
+      self.encryptUsers(g, res, client, sender, action);
+    } else {
+      return false;
+    }
+
+    var body = "Admin Action Sent."
+    var resp = '<Response><Message><Body>' + body + '</Body></Message></Response>';
+        res.status(200)
+        .contentType('text/xml')
+        .send(resp);
+
+    return true;
+  };
+
+};
+
+module.exports = AdminActions;
