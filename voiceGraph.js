a0391457bc1b6c73a9609284d4839aecb5df8872
MIchael LeGrand <michael_legrand@yahoo.com>
2017-04-22 14:32:39 -0400

    Fixing syntax.

--- voiceGraph.js
+++ voiceGraph.js
@@ -1,72 +1,55 @@
 var voiceGraph = {
-    "id": "registration",
-    "children": [
-        {
-            "id": "welcome",
-            "children": [
-                {
-                    "id": "help",
-                    "children": [
-                        {
-                            "id":"van"
-                            "children": [
-                                {
-                                    "id": "help"
-                                }
-                            ]
-                        }
-                        {
-                            "id":"report"
-                            "children": [
-                                {
-                                    "id": "help"
-                                }
-                            ]
-                        },
-                        {
-                        
-                            "id":"info"
-                            "children": [
-                                {
-                                    "id": "help"
-                                }
-                            ]
-                        },
-                        {
-                            "id":"leave"
-                            "children": [
-                                {
-                                    "id" : "confirmLeave"
-                                    "children" :[
-                                        {
-                                            "id" : "help"
-                                        }           
-                                    ]
-                                },
-                                {
-                                    "id" : "confirmCancel" 
-                                    "children": [
-                                        {
-                                            "id": "help"
-                                        }
-                                    ]  
-                                }  
-                            ]
-                        }
-                    ]
-                }
-            ]
-        }, 
-        {
-            "Id": "registerZip",
-            "children": [
-                {
-                    "Id": "correctZip",
-                }, 
-                {
-                    "Id": "wrongZip",
-                }
-            ]
-        }
-    ]
-};
+	"id": "registration",
+	"children": [{
+			"id": "welcome",
+			"children": [{
+				"id": "help",
+				"children": [{
+						"id": "van",
+						"children": [{
+							"id": "help"
+						}]
+					}, {
+						"id": "report",
+						"children": [{
+							"id": "help"
+						}]
+					},
+					{
+
+						"id": "info",
+						"children": [{
+							"id": "help"
+						}]
+					},
+					{
+						"id": "leave",
+						"children": [{
+								"id": "confirmLeave",
+								"children": [{
+									"id": "help"
+								}]
+							},
+							{
+								"id": "confirmCancel",
+								"children": [{
+									"id": "help"
+								}]
+							}
+						]
+					}
+				]
+			}]
+		},
+		{
+			"Id": "registerZip",
+			"children": [{
+					"Id": "correctZip"
+				},
+				{
+					"Id": "wrongZip"
+				}
+			]
+		}
+	]
+}
